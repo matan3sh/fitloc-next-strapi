25d3ee62265c3c37255b5bd7a8b10e710a154e3e
matan3sh <matan3sh@gmail.com>
2021-04-19 17:45:29 +0300

    First commit

---
+++ frontend/pages/workouts/[slug].js
@@ -0,0 +1,7 @@
+export default function WorkoutPage() {
+  return (
+    <div>
+      <h1>My Workout</h1>
+    </div>
+  );
+}
---
+++ frontend/pages/workouts/index.js
@@ -0,0 +1,7 @@
+export default function WorkoutsPage() {
+  return (
+    <div>
+      <h1>My Workouts</h1>
+    </div>
+  );
+}
