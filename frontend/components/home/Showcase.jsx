a60c2228c7210ccf384dece34ef5e580f403e5a7
Matan Shaviro <matan3sh@gmail.com>
2021-04-22 10:03:40 +0300

    Start to implement api routes in next js

--- frontend/components/home/Showcase.jsx
+++ frontend/components/home/Showcase.jsx
@@ -3,7 +3,7 @@ import styles from 'styles/components/home/Showcase.module.css';
 const Showcase = () => {
   return (
     <div className={styles.showcase}>
-      <h1>One Life Live It Well</h1>
+      <h1>One Life, Live It Well</h1>
       <h2>Find the Best Workout in your Place</h2>
     </div>
   );
---
+++ frontend/pages/api/workouts/index.js
@@ -0,0 +1,9 @@
+const { workouts } = require('./data.json');
+
+export default (req, res) => {
+  if (req.method === 'GET') res.status(200).json(workouts);
+  else {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).json({ message: `Method ${req.method} is not allowed` });
+  }
+};
