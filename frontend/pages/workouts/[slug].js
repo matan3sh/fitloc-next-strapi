62f2f6fdb29237e43754e74fae709cd111d5a951
matan3sh <matan3sh@gmail.com>
2021-04-27 17:47:52 +0300

    Implement workout page with fetching data

--- frontend/pages/workouts/[slug].js
+++ frontend/pages/workouts/[slug].js
@@ -1,9 +1,39 @@
 import { Layout } from 'components/layout';
 
-export default function WorkoutPage() {
+import { API_URL } from 'config';
+
+const WorkoutPage = ({ workout }) => {
+  debugger;
   return (
     <Layout>
       <h1>My Workout</h1>
     </Layout>
   );
+};
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/workouts/${slug}`);
+  const workout = await res.json();
+
+  return {
+    props: {
+      workout: workout[0],
+    },
+    revalidate: 1,
+  };
 }
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/workouts`);
+  const workouts = await res.json();
+
+  const paths = workouts.map((workout) => ({
+    params: { slug: workout.slug },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export default WorkoutPage;
