cb74ca86bd30193d75061e95375b494ed530c7d9
Matan Shaviro <matan3sh@gmail.com>
2021-05-04 20:42:16 +0300

    Finish single workout page

--- frontend/pages/workouts/[slug].js
+++ frontend/pages/workouts/[slug].js
@@ -1,12 +1,51 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import { Layout } from 'components/layout';
 
 import { API_URL } from 'config';
+import { FaPencilAlt, FaTimes } from 'react-icons/fa';
+import styles from 'styles/pages/workouts/Workout.module.css';
 
 const WorkoutPage = ({ workout }) => {
-  debugger;
+  const deleteWorkout = () => console.log('Delete');
+
   return (
     <Layout>
-      <h1>My Workout</h1>
+      <div className={styles.workout}>
+        <div className={styles.controls}>
+          <Link href={`/workouts/edit/${workout.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Workout
+            </a>
+          </Link>
+          <a href='#' className={styles.delete} onClick={deleteWorkout}>
+            <FaTimes /> Delete Workout
+          </a>
+        </div>
+
+        <span>
+          {workout.date} at {workout.time}
+        </span>
+        <h1>{workout.name}</h1>
+        {workout.image && (
+          <div className={styles.image}>
+            <Image src={workout.image} width={960} height={600} />
+          </div>
+        )}
+
+        <h3>Level:</h3>
+        <p>{workout.minimumSkill}</p>
+        <h3>Description:</h3>
+        <p>{workout.description}</p>
+        <h3>Duration & Price:</h3>
+        <p>
+          {workout.duration}min at ${workout.price}
+        </p>
+
+        <Link href='/workouts'>
+          <a className={styles.back}>{'<'} Back</a>
+        </Link>
+      </div>
     </Layout>
   );
 };
