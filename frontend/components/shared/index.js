ff9b64fe21a430a44dfab2c95833cf4c192a228c
Matan Shaviro <matan3sh@gmail.com>
2021-05-12 13:42:55 +0300

    Implement app pagination

--- frontend/components/shared/index.js
+++ frontend/components/shared/index.js
@@ -1,2 +1,3 @@
 export { default as Modal } from './Modal';
 export { default as ImageUpload } from './ImageUpload';
+export { default as Pagination } from './Pagination';
--- frontend/pages/workouts/index.js
+++ frontend/pages/workouts/index.js
@@ -1,9 +1,10 @@
 import { Layout } from 'components/layout';
 import { WorkoutItem } from 'components/home';
+import { Pagination } from 'components/shared';
 
-import { API_URL } from 'config';
+import { API_URL, PER_PAGE } from 'config';
 
-const WorkoutsPage = ({ workouts }) => {
+const WorkoutsPage = ({ workouts, page, total }) => {
   return (
     <Layout>
       <h1>Workouts</h1>
@@ -12,16 +13,28 @@ const WorkoutsPage = ({ workouts }) => {
       {workouts.map((workout) => (
         <WorkoutItem key={workout.id} workout={workout} />
       ))}
+
+      <Pagination page={page} total={total} />
     </Layout>
   );
 };
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/workouts?_sort=date:ASC`);
-  const workouts = await res.json();
+export async function getServerSideProps({ query: { page = 1 } }) {
+  // Calc start page
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
+
+  // Fetch total/count
+  const totalRes = await fetch(`${API_URL}/workouts/count`);
+  const total = await totalRes.json();
+
+  // Fetch workouts
+  const workoutsRes = await fetch(
+    `${API_URL}/workouts?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
+  );
+  const workouts = await workoutsRes.json();
+
   return {
-    props: { workouts },
-    revalidate: 1,
+    props: { workouts, page: +page, total },
   };
 }
 
