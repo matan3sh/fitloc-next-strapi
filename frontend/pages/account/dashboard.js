7623c0c113263c3b28445f22457bcb6f6c52014d
Matan Shaviro <matan3sh@gmail.com>
2021-05-13 13:59:19 +0300

    Fetching current user workouts from server

--- frontend/pages/account/dashboard.js
+++ frontend/pages/account/dashboard.js
@@ -1,11 +1,29 @@
 import { Layout } from 'components/layout';
+import { API_URL } from 'config';
 
-const DashboardPage = () => {
+import { parseCookies } from 'helpers';
+
+const DashboardPage = ({ workouts }) => {
   return (
     <Layout>
       <h2>I am Dashboard Page</h2>
     </Layout>
   );
 };
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+  const res = await fetch(`${API_URL}/workouts/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const workouts = await res.json();
+
+  return {
+    props: { workouts },
+  };
+}
 
 export default DashboardPage;
