7d1db52f13a1bbc93f5a11bf22dc3e2b201c8a45
Matan Shaviro <matan3sh@gmail.com>
2021-05-13 12:42:37 +0300

    Set cookie in headers server side

--- frontend/context/AuthContext.js
+++ frontend/context/AuthContext.js
@@ -24,7 +24,6 @@ export const AuthProvider = ({ children }) => {
     });
 
     const data = await res.json();
-    console.log(data);
 
     if (res.ok) {
       setUser(data.user);
--- frontend/pages/api/login.js
+++ frontend/pages/api/login.js
@@ -1,4 +1,5 @@
 import { API_URL } from 'config';
+import cookie from 'cookie';
 
 export default async (req, res) => {
   if (req.method === 'POST') {
@@ -17,7 +18,18 @@ export default async (req, res) => {
     const data = await strapiRes.json();
 
     if (strapiRes.ok) {
-      // @Todo - Set Cookie
+      // Set Cookie
+      res.setHeader(
+        'Set-Cookie',
+        cookie.serialize('token', data.jwt, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== 'development',
+          maxAge: 60 * 60 * 24 * 7, //1 week
+          sameSite: 'strict',
+          path: '/',
+        })
+      );
+
       res.status(200).json({ user: data.user });
     } else {
       res
