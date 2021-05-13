afd429e3b11aa3a0f612eed65fa9414fa373d29d
Matan Shaviro <matan3sh@gmail.com>
2021-05-13 13:15:13 +0300

    Implement logout functionality with destroy cookie

--- frontend/context/AuthContext.js
+++ frontend/context/AuthContext.js
@@ -8,6 +8,12 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
 
+  const router = useRouter();
+
+  useEffect(() => {
+    checkUserLoggedIn();
+  }, []);
+
   // Register User
   const register = async (user) => {
     console.log(user);
@@ -27,6 +33,7 @@ export const AuthProvider = ({ children }) => {
 
     if (res.ok) {
       setUser(data.user);
+      router.push('/account/dashboard');
     } else {
       setError(data.message);
       setError(null);
@@ -35,12 +42,26 @@ export const AuthProvider = ({ children }) => {
 
   // Logout User
   const logout = async () => {
-    console.log('Logout');
+    const res = await fetch(`${NEXT_URL}/api/logout`, {
+      method: 'POST',
+    });
+
+    if (res.ok) {
+      setUser(null);
+      router.push('/');
+    }
   };
 
   // Check if user is logged in
   const checkUserLoggedIn = async () => {
-    console.log('Check');
+    const res = await fetch(`${NEXT_URL}/api/user`);
+    const data = await res.json();
+
+    if (res.ok) {
+      setUser(data.user);
+    } else {
+      setUser(null);
+    }
   };
 
   return (
---
+++ frontend/pages/account/dashboard.js
@@ -0,0 +1,11 @@
+import { Layout } from 'components/layout';
+
+const DashboardPage = () => {
+  return (
+    <Layout>
+      <h2>I am Dashboard Page</h2>
+    </Layout>
+  );
+};
+
+export default DashboardPage;
